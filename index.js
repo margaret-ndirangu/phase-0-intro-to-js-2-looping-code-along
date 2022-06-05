5820ed9d288749335bf8f4f610eeea42141704bd
Margaret <wanjirundirangu43@gmail.com>
2022-06-05 14:32:16 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+let messages = [];
+function writeCards(names, eventName){
+    for(let i = 0;i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        
+    }
+    return messages;
+};
+function countDown(){
+  let count = 10;
+  while(count > -1){
+      console.log(count--)
+  }
+}
